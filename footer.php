7479181f4762348f62084580e534cc5ee54159aa
Flávio <pereiracruz2002@gmail.com>
2014-10-02 22:48:55 -0300

    tela login

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
-				<div class="col-md-1 col-xs-12 col-sm-12">
+
+				<div class="col-md-1 col-xs-1 col-sm-1">
 		
 				</div>
 		</div>
--- header.php
+++ header.php
@@ -16,5 +16,67 @@
     <body>
     	<div class="container">
     		<div class="row">
-				<div class="col-md-1 col-xs-12 col-sm-12">
+				<div class="col-md-1 col-xs-1 col-sm-1">
 				</div>
+                    <div class="col-md-10 col-xs-10 col-sm-10">
+        <div id="conteudo"> 
+            <div class="row">
+                <div class="col-md-12 col-xs-12 col-sm-12">
+                <div class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+
+            <a class="navbar-brand logo" href="#"><img src="imagens/logoFIS.png" class="img-responsive" /></a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Página Inicial</a></li>
+              <li><a href="#">Suporte</a></li>
+              <li><a href="#">Contatos</a></li>
+              <li><a href="#">F.A.C</a></li>
+            </ul>
+            <div id="topo"class="pull-right">
+                <div class="menuTopo">
+                    <div class="acesso">
+                        <div id="login">
+                            <div id="btnFechar" onclick="login('fechar');"><img src="javascript/jcarousel/btnFechar.png" width="14" height="14" /></div>
+                            
+                            <span class="formLabel">Login de usuário</span>
+                            <input type="text" id="txtUsuario" class="textBox" placeholder="Digite o seu usuário" maxlength="20" />
+                            
+                            <span class="formLabel">Senha de acesso</span>
+                            <input type="password" id="txtSenha" class="textBox" placeholder="Digite a sua senha" maxlength="20" />
+                            
+                            <div class="btnLogar">FAZER LOGIN</div>
+                            
+                            <div class="notice">Esqueceu a senha <b>Clique Aqui</b></div>
+                            <div class="notice">Alterar a senha <b>Clique Aqui</b></div>
+                        </div>
+                        
+                        
+                        <!-- Nao está logado -- > 
+                        <div class="btnAcessarPortal" onclick="login('mostrar');">Acessar o portal</div>
+                        <div class="btnAcessoPortal">Quero ter acesso ao portal</div>
+                        <!-- -->
+                        
+                        <!-- Está logado -->
+                        <div class="btnSair">Sair</div>
+                        <div class="btnLogado">Olá, <b>Gilberto Colombo</b> (<span class="dominio">Admin</span>)</div>
+                        <!-- -->
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </div>
+
+                    <hr />
+                </div>
